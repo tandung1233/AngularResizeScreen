20232c8325d771fc810affbc0b128f0d9d2e92a6
tandung <nguyentandung9111@gmail.com>
2023-03-27 17:10:52 +0700

    add(content): Đây là ví dụ mẫu hãy tham khảo nó

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TestWindowScreen';
+export class AppComponent implements OnInit {
+  count: number = 1;
+
+  ngOnInit(): void {
+    this.updateCount(window.innerWidth);
+    this.getPagination(this.count);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    console.log("%cĐã chạy onResize","color: blue");
+    this.updateCount(event.target.innerWidth);
+    this.getPagination(this.count);
+  }
+
+  private updateCount(width: number) {
+    this.count = (width <= 1024) ? this.count : 2;
+    this.count = (width > 1024) ? this.count : 1;
+  }
+
+  getPagination(count: number): void {
+    console.log(count);
+  }
 }
